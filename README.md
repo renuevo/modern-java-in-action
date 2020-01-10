a28292140a40fc00e0e819c92a0ece4f3c2baf09
renuevo <renuevo0110@gmail.com>
2020-01-10 22:14:28 +0900

    Modern Java In Action Project Init :tada:

---
+++ README.md
@@ -0,0 +1,7 @@
+# Modern Java In Action 정리  
+[모던 자바 인 액션](https://book.naver.com/bookdb/book_detail.nhn?bid=15261103)  
+
+
+
+
+## Chapter
---
+++ src/main/java/com/github/renuevo/ModernJavaInActionApplication.java
@@ -0,0 +1,18 @@
+package com.github.renuevo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernJavaInActionApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModernJavaInActionApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
